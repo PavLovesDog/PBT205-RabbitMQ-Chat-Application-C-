4ba463902f4d76504d421e45dbab0fc5e743d67f
PavLovesDog <pavlovesdoge@gmail.com>
2022-02-23 16:22:58 +1100

    Sending & Receiving
    Send.cs and Receive.cs simple form set up.
    
    - create connection, channel & a queue
    - currently operates through terminal

--- Receive/Receive.cs
+++ Receive/Receive.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
 
-namespace Receive
+class Receive
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        // similar to Send.cs. Open a channel, a connection & declare a queue to consume from
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using(var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
         {
-            Console.WriteLine("Hello World!");
+            channel.QueueDeclare(queue: "hello", // queue name MUST match that of Send.cs
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+            };
+            channel.BasicConsume(queue: "hello",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
         }
     }
 }
--- Send/Send.cs
+++ Send/Send.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using RabbitMQ.Client;
+using System.Text;
 
-namespace Send
+class Send
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        // Create the connection to server
+        var factory = new ConnectionFactory() {HostName = "localhost"}; //connect to a RabbitMQ node on the local machine i.e "localhost" If we wanted to connect to a node on a different machine we specify its hostname or IP address here.
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
         {
-            Console.WriteLine("Hello World!");
+            // Create channel for API
+            channel.QueueDeclare(queue: "hello",
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+                                       
+            
+            string message = "...Hello Charlie!"; // Message to "send"/ Add to queue
+            var body = Encoding.UTF8.GetBytes(message);
+
+            // declare a queue to send to, then publish a message to the queue
+            channel.BasicPublish(exchange: "",
+                                            routingKey: "hello",
+                                            basicProperties: null,
+                                            body: body);
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
         }
     }
 }
